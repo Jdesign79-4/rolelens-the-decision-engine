2487337f20d3a377d9025e22d8ef6dc48a2448d0
Jeremy William Martin <jdesign79@gmail.com>
2026-03-14 04:40:06 +0000

    File changes

--- src/pages/RoleLens.jsx
+++ src/pages/RoleLens.jsx
@@ -1224,6 +1224,8 @@ function RoleLensContent() {
               </motion.div>
             )}
 
+            <BambooGroveStability data={currentJob.stability} />
+
             {/* Intelligence Cards Grid */}
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-6">
               {visibleWidgets.includes('stability') && (
